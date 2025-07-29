92f97d4e5afd5c1e1092ab739e803fa661705c43
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-29 22:00:25 +0330

    update: StockByTypeChart with rechart

--- src/components/charts/StockByTypeChart.tsx
+++ src/components/charts/StockByTypeChart.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+
+const data = [
+  { type: "A2", stock: 80 },
+  { type: "A3", stock: 100 },
+  { type: "A4", stock: 120 },
+];
 
 const StockByTypeChart = () => {
   return (
-    <div>StockByTypeChart</div>
-  )
-}
+    <div className="w-full h-72">
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="type" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="stock" fill="#3b82f6" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
 
-export default StockByTypeChart
+export default StockByTypeChart;
