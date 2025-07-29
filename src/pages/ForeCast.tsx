7de8a3b1071e0abc8bdc091aa7816d5fcda26b24
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-29 21:50:59 +0330

    update: add ForcastTable component

--- src/pages/ForeCast.tsx
+++ src/pages/ForeCast.tsx
@@ -1,5 +1,13 @@
 // src/pages/Forecast.tsx
+import ForecastTable from "../components/ui/ForecastTable";
 import Layout from "../components/layout/Layout";
+
+const sampleDailyData = [
+  { date: "1403/05/08", type: "A3", size: "16", predictedPrice: 26900 },
+  { date: "1403/05/09", type: "A3", size: "16", predictedPrice: 27100 },
+  { date: "1403/05/10", type: "A3", size: "16", predictedPrice: 27000 },
+];
+
 
 const Forecast = () => {
   return (
@@ -10,8 +18,8 @@ const Forecast = () => {
         <div className="bg-white p-4 rounded shadow">
           {/* جدول پیش‌بینی قیمت */}
           <h2 className="font-semibold mb-2">پیش‌بینی روزانه</h2>
-          <div className="text-sm text-gray-500">اینجا جدول روزانه میاد</div>
-        </div>
+          <ForecastTable data={sampleDailyData} />
+          </div>
         <div className="bg-white p-4 rounded shadow">
           {/* نمودار قیمت */}
           <h2 className="font-semibold mb-2">نمودار روند پیش‌بینی</h2>
