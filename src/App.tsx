566218ba3398536ea8f7c381b7b943652786f5b5
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-29 21:50:10 +0330

    update: add link page ForeCast

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Analytics from "./pages/Analytics";
+import Forecast from "./pages/ForeCast";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />}  />
         <Route path="/analytics" element={<Analytics />} />
+        <Route path="/forecast" element={<Forecast />} />
       </Routes>
     </Router>
   );
