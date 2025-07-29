c1d6cdfef92821abdf58fc70992034350632c6fe
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-29 19:08:55 +0330

    install prettier and update

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-red-700'>
+        <p className="text-red-700">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-       
-        </header>
+      </header>
     </div>
   );
 }
