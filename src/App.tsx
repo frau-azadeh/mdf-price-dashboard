7604785f0adb32a3e65bab81e3766ada27e30fba
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-29 21:42:13 +0330

    update : add App.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import "./App.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
-import PriceAnalysis from "./pages/Analytics";
+import Analytics from "./pages/Analytics";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/analytics" element={<PriceAnalysis />} />
+        <Route path="/" element={<Dashboard />}  />
+        <Route path="/analytics" element={<Analytics />} />
       </Routes>
     </Router>
   );
