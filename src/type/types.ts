b96a3e22a8b30a3b80e278e82612a5db6e24f257
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-30 11:43:56 +0330

    add: add type

---
+++ src/type/types.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    id: number;
+    name: string;
+    type: string;
+    size: string;
+    form: string;
+    standard: string;
+    warehouse: string;
+    unit: string;
+    price: number;
+    lastPriceDate: string;
+  }
+  
