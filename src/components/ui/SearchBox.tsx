ab5736f708eab7071a64b8d9316923c9a9aa447e
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-30 11:37:03 +0330

    update: change ui

--- src/components/ui/SearchBox.tsx
+++ src/components/ui/SearchBox.tsx
@@ -7,13 +7,16 @@ type Props = {
 
 const SearchBox: FC<Props> = ({ value, onChange }) => {
   return (
+
     <input
       type="text"
       placeholder="جستجو بر اساس نام یا نوع..."
       value={value}
       onChange={(e) => onChange(e.target.value)}
-      className="w-full md:w-72 border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring focus:ring-blue-200"
+      className="w-full  border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring focus:ring-blue-200"
     />
+
+
   );
 };
 
