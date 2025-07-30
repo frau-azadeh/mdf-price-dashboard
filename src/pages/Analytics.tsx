39f9a545c9d2a30c9b9d827a4b1df0af8a2310d3
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-30 20:10:58 +0330

    update with api

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -7,6 +7,8 @@ import PriceLineChart from "../components/charts/PriceLineChart";
 import { useEffect, useState } from "react";
 import { getProducts } from "../services/api";
 import { Product } from "../type/types";
+import Pagination from "../components/ui/Pagination";
+
 
 const Analytics = () => {
   const [query, setQuery] = useState("");
@@ -44,18 +46,6 @@ const Analytics = () => {
   return (
     <Layout>
       <h1 className="text-2xl font-bold mb-4">تحلیل قیمت میلگرد</h1>
-
-      <div className="bg-white rounded-lg shadow p-4 space-y-4">
-        <SearchBox value={query} onChange={setQuery} />
-        <FilterPanel
-          type={type}
-          size={size}
-          onTypeChange={setType}
-          onSizeChange={setSize}
-        />
-        <Table data={filtered} />
-      </div>
-
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-6">
         <div className="bg-white rounded-lg shadow p-4">
           <h2 className="text-lg font-semibold mb-2">نمودار تغییر قیمت</h2>
@@ -66,6 +56,24 @@ const Analytics = () => {
           <StockByTypeChart />
         </div>
       </div>
+      <div className="bg-white rounded-lg shadow p-4 space-y-4">
+        <SearchBox value={query} onChange={setQuery} />
+        <FilterPanel
+          type={type}
+          size={size}
+          onTypeChange={setType}
+          onSizeChange={setSize}
+        />
+        <Table data={paginatedData}  startIndex={startIndex}/>
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={setCurrentPage}
+                />
+
+      </div>
+
+
     </Layout>
   );
 };
