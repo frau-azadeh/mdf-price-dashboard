97249666c651791ec1acf0f27fa92385ffa1a310
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-30 16:45:35 +0330

    update : add table date with moment-jalaali

--- src/components/ui/Table.tsx
+++ src/components/ui/Table.tsx
@@ -1,14 +1,6 @@
+import moment from "moment-jalaali";
 import { FC } from "react";
-
-type Product = {
-  id: number;
-  name: string;
-  type: string;
-  size: string;
-  stock: number;
-  price: number;
-  lastUpdate: string;
-};
+import { Product } from "../../type/types";
 
 type TableProps = {
   data: Product[];
@@ -24,23 +16,39 @@ const Table: FC<TableProps> = ({ data }) => {
             <th className="p-3">نام</th>
             <th className="p-3">نوع</th>
             <th className="p-3">سایز</th>
-            <th className="p-3">موجودی</th>
+            <th className="p-3">فرم</th>
+            <th className="p-3">استاندارد</th>
+            <th className="p-3">انبار</th>
+            <th className="p-3">واحد</th>
             <th className="p-3">قیمت (تومان)</th>
             <th className="p-3">آخرین بروزرسانی</th>
           </tr>
         </thead>
         <tbody>
-          {data.map((item, i) => (
-            <tr key={item.id} className="border-b hover:bg-gray-50">
-              <td className="p-3">{i + 1}</td>
-              <td className="p-3">{item.name}</td>
-              <td className="p-3">{item.type}</td>
-              <td className="p-3">{item.size}</td>
-              <td className="p-3">{item.stock}</td>
-              <td className="p-3">{item.price.toLocaleString()}</td>
-              <td className="p-3">{item.lastUpdate}</td>
-            </tr>
-          ))}
+          {data.map((item, i) => {
+            let formattedDate = "-";
+            if (item.lastPriceDate) {
+              formattedDate = moment(
+                item.lastPriceDate,
+                "jYYYY-jMM-jDDTHH:mm:ss"
+              ).format("jYYYY/jMM/jDD");
+            }
+
+            return (
+              <tr key={item.id} className="border-b hover:bg-gray-50">
+                <td className="p-3">{i + 1}</td>
+                <td className="p-3">{item.name}</td>
+                <td className="p-3">{item.type}</td>
+                <td className="p-3">{item.size}</td>
+                <td className="p-3">{item.form || "-"}</td>
+                <td className="p-3">{item.standard || "-"}</td>
+                <td className="p-3">{item.warehouse || "-"}</td>
+                <td className="p-3">{item.unit || "-"}</td>
+                <td className="p-3">{item.price.toLocaleString()}</td>
+                <td className="p-3">{formattedDate}</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
