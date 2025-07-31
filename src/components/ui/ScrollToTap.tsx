832954be5bde0ddc3fa15e077f7e48be30802b50
Azadeh Sharifi Soltani <designweb.azadeh@gmail.com>
2025-07-31 18:30:57 +0330

    update with api

---
+++ src/components/ui/ScrollToTap.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { ArrowUp } from 'lucide-react';
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-6 left-6 z-50 bg-[#f14646] text-white p-3 rounded-full shadow-lg hover:bg-[#7f1d1d] transition duration-300"
+          aria-label="Scroll to top"
+        >
+          <ArrowUp className="w-5 h-5" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
